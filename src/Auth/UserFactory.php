13b331cb8d9db5fb8815d626cb969d2ca515cda5
Tomáš Chochola <chocholatom1997@gmail.com>
2022-05-03 10:25:26 +0200

    Extract user factory from boilerplate

---
+++ src/Auth/UserFactory.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tomchochola\Laratchi\Auth;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<User>
+ */
+class UserFactory extends Factory
+{
+    /**
+     * @inheritDoc
+     */
+    protected $model = User::class;
+
+    /**
+     * @inheritDoc
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'email_verified_at' => Carbon::now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'locale' => resolveApp()->getLocale(),
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     */
+    public function unverified(): static
+    {
+        return $this->state(static function (): array {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+}
