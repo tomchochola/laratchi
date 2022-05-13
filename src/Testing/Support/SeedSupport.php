a4fbfc6f07bd70acff83d9497ad64811caa439ea
Tomáš Chochola <chocholatom1997@gmail.com>
2022-05-13 16:19:34 +0200

    Add SeedSupport to generate random images

---
+++ src/Testing/Support/SeedSupport.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tomchochola\Laratchi\Testing\Support;
+
+use Illuminate\Http\Testing\File;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Arr;
+
+class SeedSupport
+{
+    /**
+     * Generate random avatar url.
+     */
+    public static function randomAvatarUrl(int $width = 100): string
+    {
+        return "https://i.pravatar.cc/{$width}";
+    }
+
+    /**
+     * Generate random image url from keywords.
+     *
+     * @param array<int, string> $keywords
+     */
+    public static function imageUrl(array $keywords): string
+    {
+        $keyword = \implode(',', $keywords);
+
+        $query = \http_build_query([
+            'method' => 'search',
+            'keyword' => $keyword,
+            'itemsperpage' => 1,
+            'itemsperpage_su' => 1,
+            'itemsperpage_free' => 1,
+        ]);
+
+        $response = \file_get_contents("https://www.123rfapis.com?{$query}");
+
+        if ($response === false) {
+            return static::randomImageUrl();
+        }
+
+        $json = \json_decode($response, true);
+
+        if ($json === null) {
+            return static::randomImageUrl();
+        }
+
+        \assert(\is_array($json));
+
+        $url = Arr::get($json, '0.images.123RF.image.0.link_image') ?? Arr::get($json, '0.images.stockunlimited.image.0.link_image') ?? Arr::get($json, '0.images.freeimages.image.0.link_image');
+
+        if ($url === null) {
+            return static::randomImageUrl();
+        }
+
+        \assert(\is_string($url));
+
+        return $url;
+    }
+
+    /**
+     * Generate random image url.
+     */
+    public static function randomImageUrl(int $width = 100, ?int $height = null): string
+    {
+        if ($height !== null) {
+            return "https://picsum.photos/{$width}/{$height}";
+        }
+
+        return "https://picsum.photos/{$width}";
+    }
+
+    /**
+     * Create fake image.
+     *
+     * @param 'jpeg'|'png'|'gif'|'webp'|'wbmp'|'bmp' $extension
+     */
+    public static function fakeImage(string $extension = 'webp', int $width = 10, int $height = 10): File
+    {
+        return UploadedFile::fake()->image("image.{$extension}", $width, $height);
+    }
+}
