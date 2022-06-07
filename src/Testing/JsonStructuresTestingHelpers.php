ea616a0a024af7909cb204299cfeede26a49f150
Tomáš Chochola <chocholatom1997@gmail.com>
2022-06-07 14:05:55 +0200

    Add assertJsonApiResponse testing helper

--- src/Testing/JsonStructuresTestingHelpers.php
+++ src/Testing/JsonStructuresTestingHelpers.php
@@ -5,47 +5,72 @@ declare(strict_types=1);
 namespace Tomchochola\Laratchi\Testing;
 
 use Illuminate\Support\Arr;
+use Illuminate\Testing\TestResponse;
 
 trait JsonStructuresTestingHelpers
 {
     /**
      * Resource json structure.
      *
+     * @param array<mixed> $attributes
+     *
      * @return array<mixed>
      */
-    public function jsonStructureResource(): array
+    public function jsonStructureResource(array $attributes = []): array
     {
-        return [
+        $structure = [
             'id',
             'type',
             'slug',
         ];
+
+        if (\count($attributes) > 0) {
+            $structure['attributes'] = $attributes;
+        }
+
+        return $structure;
     }
 
     /**
      * Relationship json structure.
      *
+     * @param array<mixed> $meta
+     *
      * @return array<mixed>
      */
-    public function jsonStructureRelationship(): array
+    public function jsonStructureRelationship(array $meta = []): array
     {
-        return [
+        $structure = [
             'data' => $this->jsonStructureResource(),
         ];
+
+        if (\count($meta) > 0) {
+            $structure['meta'] = $meta;
+        }
+
+        return $structure;
     }
 
     /**
      * Relationships json structure.
      *
+     * @param array<mixed> $meta
+     *
      * @return array<mixed>
      */
-    public function jsonStructureRelationships(): array
+    public function jsonStructureRelationships(array $meta = []): array
     {
-        return [
+        $structure = [
             'data' => [
                 '*' => $this->jsonStructureResource(),
             ],
         ];
+
+        if (\count($meta) > 0) {
+            $structure['meta'] = $meta;
+        }
+
+        return $structure;
     }
 
     /**
@@ -55,15 +80,13 @@ trait JsonStructuresTestingHelpers
      */
     public function jsonStructureMe(bool $withDatabaseToken = false): array
     {
-        $structure = \array_merge($this->jsonStructureResource(), [
-            'attributes' => [
-                'name',
-                'email',
-                'email_verified_at',
-                'locale',
-                'created_at',
-                'updated_at',
-            ],
+        $structure = $this->jsonStructureResource([
+            'name',
+            'email',
+            'email_verified_at',
+            'locale',
+            'created_at',
+            'updated_at',
         ]);
 
         if ($withDatabaseToken) {
@@ -80,13 +103,11 @@ trait JsonStructuresTestingHelpers
      */
     public function jsonStructureDatabaseToken(bool $withBearer = false): array
     {
-        $structure = \array_merge($this->jsonStructureResource(), [
-            'attributes' => [
-                'provider',
-                'auth_id',
-                'created_at',
-                'updated_at',
-            ],
+        $structure = $this->jsonStructureResource([
+            'provider',
+            'auth_id',
+            'created_at',
+            'updated_at',
         ]);
 
         \assert(\is_array($structure['attributes']));
@@ -96,5 +117,75 @@ trait JsonStructuresTestingHelpers
         }
 
         return $structure;
+    }
+
+    /**
+     * Json api response assert.
+     *
+     * @param array<mixed> $dataStructure
+     * @param array<mixed> $includedStructure
+     */
+    public function assertJsonApiResponse(TestResponse $response, array $dataStructure = [], int $includedCount = 0, array $includedStructure = []): void
+    {
+        if (\count($dataStructure) === 0) {
+            $dataStructure = $this->jsonStructureResource();
+        }
+
+        if (\count($includedStructure) === 0) {
+            $includedStructure = $this->jsonStructureResource();
+        }
+
+        $response->assertJsonStructure([
+            'data' => $dataStructure,
+        ]);
+
+        if ($includedCount <= 0) {
+            $response->assertJsonMissingPath('included');
+        } else {
+            $response->assertJsonStructure([
+                'included' => [
+                    '*' => $includedStructure,
+                ],
+            ]);
+
+            $response->assertJsonCount($includedCount, 'included');
+        }
+    }
+
+    /**
+     * Json api collection response assert.
+     *
+     * @param array<mixed> $dataStructure
+     * @param array<mixed> $includedStructure
+     */
+    public function assertJsonApiCollectionResponse(TestResponse $response, int $dataCount, array $dataStructure = [], int $includedCount = 0, array $includedStructure = []): void
+    {
+        if (\count($dataStructure) === 0) {
+            $dataStructure = $this->jsonStructureResource();
+        }
+
+        if (\count($includedStructure) === 0) {
+            $includedStructure = $this->jsonStructureResource();
+        }
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => $dataStructure,
+            ],
+        ]);
+
+        $response->assertJsonCount($dataCount, 'data');
+
+        if ($includedCount <= 0) {
+            $response->assertJsonMissingPath('included');
+        } else {
+            $response->assertJsonStructure([
+                'included' => [
+                    '*' => $includedStructure,
+                ],
+            ]);
+
+            $response->assertJsonCount($includedCount, 'included');
+        }
     }
 }
