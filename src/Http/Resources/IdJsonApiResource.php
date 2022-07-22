1960d5488b672eab4a11cdbe4cf8bc40877fc8fc
Tomáš Chochola <chocholatom1997@gmail.com>
2022-07-22 10:30:04 +0200

    Add IdJsonApiResource

---
+++ src/Http/Resources/IdJsonApiResource.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tomchochola\Laratchi\Http\Resources;
+
+class IdJsonApiResource extends ModelJsonApiResource
+{
+    /**
+     * @inheritDoc
+     */
+    public function getAttributes(): array
+    {
+        return [];
+    }
+}
