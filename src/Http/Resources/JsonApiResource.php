bf1f316b9c3cc8a0a23f195b98fe451b7f823ce2
Tomáš Chochola <chocholatom1997@gmail.com>
2022-09-21 17:36:58 +0200

    Fix meta in included section of json:api response

--- src/Http/Resources/JsonApiResource.php
+++ src/Http/Resources/JsonApiResource.php
@@ -22,7 +22,15 @@ abstract class JsonApiResource extends JsonResource
     public static function withIncluded(iterable $included, array &$map, Request $request): void
     {
         foreach ($included as $resource) {
-            $map[$resource->getKey().':'.$resource->getType()] = $resource->toArray($request);
+            $data = $resource->getArray($request);
+
+            unset($data['meta']);
+
+            if (\array_key_exists('attributes', $data) === false && \array_key_exists('relationships', $data) === false) {
+                continue;
+            }
+
+            $map[$resource->getKey().':'.$resource->getType()] = \array_merge_recursive($map[$resource->getKey().':'.$resource->getType()] ?? [], $data);
 
             static::withIncluded($resource->getIncluded(), $map, $request);
         }
@@ -96,16 +104,6 @@ abstract class JsonApiResource extends JsonResource
     }
 
     /**
-     * Get meta.
-     *
-     * @return array<string, mixed>
-     */
-    public function getMeta(): array
-    {
-        return [];
-    }
-
-    /**
      * Get resource header.
      *
      * @return array{id: string, slug: string, type: string, meta?: array<mixed>}
@@ -134,18 +132,22 @@ abstract class JsonApiResource extends JsonResource
      */
     public function toArray(mixed $request): array|ArrayableContract|JsonSerializable
     {
+        return $this->getArray($request);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @return array<string, mixed>
+     */
+    public function getArray(Request $request): array
+    {
         $data = $this->getHeader();
 
         $attributes = $this->filter($this->getAttributes());
 
         if (\count($attributes) > 0) {
             $data['attributes'] = $attributes;
-        }
-
-        $meta = $this->filter($this->getMeta());
-
-        if (\count($meta) > 0) {
-            $data['meta'] = $meta;
         }
 
         $relationships = $this->filter($this->getRelationships());
