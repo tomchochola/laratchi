6f378fa60f6c9ff3e325a87b106c6a7b25f61b94
Tomáš Chochola <chocholatom1997@gmail.com>
2023-06-21 20:11:15 +0200

    Generate unsafe e-mails

--- src/Auth/UserFactory.php
+++ src/Auth/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'email' => fake()->unique()->email(),
             'email_verified_at' => resolveDate()->now(),
             'password' => Str::random(),
             'remember_token' => Str::random(),
