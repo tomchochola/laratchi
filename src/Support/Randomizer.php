107bd8165e4bd2d874c38dc10dceff5d75028230
Tomáš Chochola <chocholatom1997@gmail.com>
2023-08-28 11:50:32 +0200

    Add Randomizer

---
+++ src/Support/Randomizer.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tomchochola\Laratchi\Support;
+
+use Illuminate\Support\Str;
+
+class Randomizer
+{
+    /**
+     * Generate random token.
+     */
+    public static function token(int $length = 64): string
+    {
+        return Str::random($length);
+    }
+
+    /**
+     * Generate random code.
+     */
+    public static function numeric(int $length = 6, bool $startWithZero = true): string
+    {
+        $code = (string) \random_int($startWithZero ? 0 : 1, 9);
+
+        for ($i = 1; $i < $length; ++$i) {
+            $code = $code.\random_int(0, 9);
+        }
+
+        return $code;
+    }
+
+    /**
+     * Generate random token/code from source.
+     *
+     * @param array<string> $source
+     */
+    public static function source(int $length = 6, array $source = []): string
+    {
+        $code = '';
+
+        if (\count($source) === 0) {
+            $source = \range('a', 'z');
+        }
+
+        $last = \count($source) - 1;
+
+        for ($i = 0; $i < $length; ++$i) {
+            $code = $code.$source[\random_int(0, $last)];
+        }
+
+        return $code;
+    }
+}
