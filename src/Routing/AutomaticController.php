eff79c2cad15626efe67f1316ef4a374bea561d2
Tomáš Chochola <chocholatom1997@gmail.com>
2023-08-28 17:09:04 +0200

    Add AutomaticController

---
+++ src/Routing/AutomaticController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tomchochola\Laratchi\Routing;
+
+use Illuminate\Routing\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Tomchochola\Laratchi\Support\Resolver;
+use Tomchochola\Laratchi\Support\Typer;
+
+class AutomaticController extends Controller
+{
+    /**
+     * @inheritDoc
+     *
+     * @param array<mixed> $parameters
+     */
+    public function callAction(mixed $method, mixed $parameters): Response
+    {
+        if (Resolver::resolveRequest()->isMethodSafe()) {
+            return parent::callAction($method, $parameters);
+        }
+
+        return Typer::assertInstance(
+            Resolver::resolveDatabaseManager()
+                ->connection()
+                ->transaction(fn (): Response => parent::callAction($method, $parameters)),
+            Response::class,
+        );
+    }
+}
