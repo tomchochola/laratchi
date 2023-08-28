9ca7c23798abfd58bd775ffc8798a4c2647873da
Tomáš Chochola <chocholatom1997@gmail.com>
2023-08-28 17:08:52 +0200

    Add DataUri encoding

---
+++ src/Encoding/DataUri.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tomchochola\Laratchi\Encoding;
+
+class DataUri
+{
+    /**
+     * Encode data to data uri.
+     */
+    public static function encode(string $mime, string $data): string
+    {
+        return 'data:'.$mime.';base64,'.\base64_encode($data);
+    }
+}
